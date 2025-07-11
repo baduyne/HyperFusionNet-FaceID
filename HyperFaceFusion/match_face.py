2bb606291c4155ce3df3473e7683eef24c518411
baduyne <baduynguyen3172001@gmail.com>
2025-07-12 01:10:46 +0700

    evaluation

--- HyperFaceFusion/match_face.py
+++ HyperFaceFusion/match_face.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 import json
 import cv2
-
+import yaml
+from model import *
 # Preprocessing ảnh thành tensor [1,1,H,W]
 def preprocess_single_image(img_path, img_size=(128, 128)):
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
@@ -59,49 +60,47 @@ def find_match(model, ir_path, vis_path, database_embeddings, device, threshold=
             matched_id = person_id
 
     if max_similarity >= threshold:
-        print(f"[MATCHED] ID: {matched_id}, similarity: {max_similarity:.4f}")
         return matched_id, max_similarity
     else:
-        print(f"[NO MATCH] Highest similarity: {max_similarity:.4f}")
         return "Not Found", max_similarity
 
-# def run():
-#     # Load configuration
-#     with open('configs.yaml', 'r') as file:
-#         config = yaml.load(file, Loader=yaml.FullLoader)  # Use safe_load for security\
-#     model_path = config['model']['path']
-#     database_filename = config['embedding']['path']
+def run():
+    # Load configuration
+    with open('configs.yaml', 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)  # Use safe_load for security\
+    model_path = config['model']['path']
+    database_filename = config['embedding']['path']
 
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     print(f"Using device: {device}")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
 
-#     model = HyperFacePipeline().to(device)
-#     if os.path.exists(model_path):
-#         print(f"Loading model from '{model_path}'...")
-#         model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
-#         print("Model loaded.")
-#     else:
-#         print(f"Model file not found at '{model_path}'.")
-#         exit()
+    model = HyperFacePipeline().to(device)
+    if os.path.exists(model_path):
+        print(f"Loading model from '{model_path}'...")
+        model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
+        print("Model loaded.")
+    else:
+        print(f"Model file not found at '{model_path}'.")
+        exit()
 
-#     database = load_database_embeddings(database_filename)
-#     if not database:
-#         print("Failed to load database.")
-#         exit()
-#     print(f"Loaded {len(database)} identities from database.")
+    database = load_database_embeddings(database_filename)
+    if not database:
+        print("Failed to load database.")
+        exit()
+    print(f"Loaded {len(database)} identities from database.")
 
-#     sample_ir = "data/RGB_Thermal/thermal/7-TD-A-2.jpg"
-#     sample_vis = "data/RGB_Thermal/rgb/7-TD-A-2.jpg"
+    sample_ir = "data/RGB_Thermal/thermal/7-TD-A-2.jpg"
+    sample_vis = "data/RGB_Thermal/rgb/7-TD-A-2.jpg"
 
-#     if not os.path.exists(sample_ir) or not os.path.exists(sample_vis):
-#         print("Missing input images for testing.")
-#         print(f"IR image: {sample_ir}")
-#         print(f"VIS image: {sample_vis}")
-#         exit()
+    if not os.path.exists(sample_ir) or not os.path.exists(sample_vis):
+        print("Missing input images for testing.")
+        print(f"IR image: {sample_ir}")
+        print(f"VIS image: {sample_vis}")
+        exit()
 
-#     result = find_match(model, sample_ir, sample_vis, database, device, threshold=0.7)
-#     print(f"\nFinal Match Result: {result}")
+    result = find_match(model, sample_ir, sample_vis, database, device, threshold=0.7)
+    print(f"\nFinal Match Result: {result}")
 
 # ================= MAIN =====================
-# if __name__ == '__main__':
-#     run()
+if __name__ == '__main__':
+    run()
